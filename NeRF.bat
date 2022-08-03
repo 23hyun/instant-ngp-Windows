3276373976e7a432a6e28acafa798267b4514209
David Goldstein <93741283+PinPointPing@users.noreply.github.com>
2022-08-02 20:47:14 -0400

    Update NeRF.bat

--- NeRF.bat
+++ NeRF.bat
@@ -1,9 +1,12 @@
 @echo off
+set title=NeRF
+title %title%
+
 set dir=data\nerf
 
 echo 1) New Project
 echo 2) Render Scene
-echo 3) Open Snapshop
+echo 3) Load Snapshop
 choice /c 123 /n /m Choice: 
 echo.
 
@@ -11,7 +14,10 @@ if errorlevel 3 goto snap
 if errorlevel 2 goto render
 if errorlevel 1 goto new
 
+
 :snap
+title %title% - Load Snapshot
+
 set /p snap=Snapshot path (*.msgpack): 
 
 build\testbed.exe --snapshot=%snap% -m nerf
@@ -20,6 +26,8 @@ pause
 exit
 
 :render
+title %title% - Render Scene
+
 set /p project=Project folder name: 
 if not exist %dir%\%project% (exit)
 if %project% == "" (exit)
@@ -39,16 +47,23 @@ rename %project%_test.mp4 %project%_render.mp4
 pause
 exit
 
+
 :new
+title %title% - New Project
+
 set path=%path%;%cd%\COLMAP
 
 set /p project=Project folder name: 
 if not exist %dir%\%project% (exit)
 if %project% == "" (exit)
 
+choice /n /c yn /m "Generate transforms? (y/n): "
+if errorlevel 2 goto run
+
 call conda activate ngp
 python scripts\colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 16 --images %dir%\%project%\%dir%\%project%
 
+timeout 1
 powershell -command "$dir = '%dir%\%project%\%dir%\%project%'; $dir = './' + $dir.replace('\', '\\\\') + '/'; (Get-Content transforms.json) -replace $dir, ('./' + '%dir%\%project%'.replace('\', '\\') + '/') | Out-File -encoding ASCII transforms.json;
 
 del colmap.db
@@ -56,6 +71,7 @@ rmdir /s /q colmap_sparse
 rmdir /s /q colmap_text
 move transforms.json %dir%\%project%
 
+:run
 build\testbed.exe --scene %dir%\%project%
 
 pause
